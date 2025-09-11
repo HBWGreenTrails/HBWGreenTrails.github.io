3dc8f77d887a01c41fec1474241d7c5cc98c5d5a
Alex Hamill <alexander.hamill@icloud.com>
2025-09-11 12:37:32 -0400

    test

--- greentrails/vite.config.ts
+++ greentrails/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import React from 'react';
 
 export default defineConfig({
-  base: './' // This ensures proper paths for GitHub Pages
+  base: '/' // This ensures proper paths for GitHub Pages
   // Other Vite configuration options can go here
 });
